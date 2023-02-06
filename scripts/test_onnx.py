2558d89ec12c5e77611a1eed1df2f5928962a763
MURAKI Taichi <muraki.taichi@gmail.com>
2023-02-06 19:00:03 +0900

    fix

--- scripts/test_onnx.py
+++ scripts/test_onnx.py
@@ -13,7 +13,7 @@ def test1(model_filename):
 
 
 def test2(model_filename):
-    ort_sess = ort.InferenceSession(model_filename)
+    ort_sess = ort.InferenceSession(str(model_filename))
     # input = np.random.random((1, 3, 28, 28), dtype=np.float32)
     size = 512
     outputs = ort_sess.run(None, {"input": np.zeros((1, 3, size, size), dtype=np.float32)})
@@ -34,6 +34,4 @@ def parse_args():
 
 if __name__ == "__main__":
     args = parse_args()
-    # model_filename = "segformer.onnx"
-    # # test1(model_filename)
-    test1(args.onnx_filename)
+    test2(args.onnx_filename)
