891c26c9f8d6bdd9b06c7950e67620dee610c7af
MURAKI Taichi <muraki.taichi@gmail.com>
2023-02-06 19:06:04 +0900

    f

--- scripts/test_onnx.py
+++ scripts/test_onnx.py
@@ -15,7 +15,7 @@ def test1(model_filename):
 def test2(model_filename):
     ort_sess = ort.InferenceSession(str(model_filename))
     # input = np.random.random((1, 3, 28, 28), dtype=np.float32)
-    size = 512
+    size = 224
     outputs = ort_sess.run(None, {"input": np.zeros((1, 3, size, size), dtype=np.float32)})
     # Print Result
     print(f"This: output={outputs}")
