4f841a02f9486b433948fc4ac4997f7ca046f4c7
MURAKI Taichi <muraki.taichi@gmail.com>
2023-02-06 18:59:18 +0900

    fix

--- scripts/test_onnx.py
+++ scripts/test_onnx.py
@@ -1,9 +1,13 @@
+from pathlib import Path
+import argparse
+
 import numpy as np
 import onnx
 import onnxruntime as ort
 
 
 def test1(model_filename):
+    print(model_filename)
     onnx_model = onnx.load(model_filename)
     onnx.checker.check_model(onnx_model)
 
@@ -17,7 +21,19 @@ def test2(model_filename):
     print(f"This: output={outputs}")
 
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--onnx_filename",
+        type=Path,
+        required=True,
+    )
+    args = parser.parse_args()
+    return args
+
+
 if __name__ == "__main__":
-    model_filename = "segformer.onnx"
-    # test1(model_filename)
-    test1(model_filename)
+    args = parse_args()
+    # model_filename = "segformer.onnx"
+    # # test1(model_filename)
+    test1(args.onnx_filename)
