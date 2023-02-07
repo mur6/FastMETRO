161185fde85322b5a8c24c83f14d19c0f04a3e2f
MURAKI Taichi <muraki.taichi@gmail.com>
2023-02-07 09:00:38 +0900

    fix

--- scripts/test_onnx.py
+++ scripts/test_onnx.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import argparse
 
+import trimesh
 from PIL import Image
 import numpy as np
 import onnx
@@ -8,20 +9,42 @@ import onnxruntime as ort
 import torch
 from torchvision import transforms
 
+from src.modeling._mano import MANO, Mesh
 
-def test1(model_filename):
-    print(model_filename)
-    onnx_model = onnx.load(model_filename)
-    onnx.checker.check_model(onnx_model)
+# def test1(model_filename):
+#     print(model_filename)
+#     onnx_model = onnx.load(model_filename)
+#     onnx.checker.check_model(onnx_model)
 
 
-def test2(model_filename):
-    ort_sess = ort.InferenceSession(str(model_filename))
-    # input = np.random.random((1, 3, 28, 28), dtype=np.float32)
-    size = 224
-    outputs = ort_sess.run(None, {"input": np.zeros((1, 3, size, size), dtype=np.float32)})
-    # Print Result
-    print(f"This: output={outputs}")
+# def test2(model_filename):
+#     ort_sess = ort.InferenceSession(str(model_filename))
+#     # input = np.random.random((1, 3, 28, 28), dtype=np.float32)
+#     size = 224
+#     outputs = ort_sess.run(None, {"input": np.zeros((1, 3, size, size), dtype=np.float32)})
+#     # Print Result
+#     print(f"This: output={outputs}")
+
+
+def visualize_mesh(*, mesh):
+    color = [102, 102, 102, 64]
+    for facet in mesh.facets:
+        # mesh.visual.face_colors[facet] = [color, color]
+        mesh.visual.face_colors[facet] = color
+    scene = trimesh.Scene()
+    scene.add_geometry(mesh)
+    # scene.add_geometry(create_point_geom(a_point, "red"))
+    scene.show()
+
+
+def make_hand_mesh(gt_vertices):
+    # gt_vertices = torch.transpose(gt_vertices, 2, 1).squeeze(0)
+    print(f"gt_vertices: {gt_vertices.shape}")
+    mano_model = MANO().to("cpu")
+    mano_faces = mano_model.layer.th_faces
+    print(f"mano_faces: {mano_faces.shape}")
+    # mesh objects can be created from existing faces and vertex data
+    return trimesh.Trimesh(vertices=gt_vertices, faces=mano_faces)
 
 
 def test3(model_filename, image_file):
@@ -49,6 +72,9 @@ def test3(model_filename, image_file):
     print(f"pred_3d_joints: {pred_3d_joints.shape}")
     print(f"pred_3d_vertices_coarse: {pred_3d_vertices_coarse.shape}")
     print(f"pred_3d_vertices_fine: {pred_3d_vertices_fine.shape}")
+    mesh = make_hand_mesh(pred_3d_vertices_fine.squeeze(0))
+    print(mesh)
+    visualize_mesh(mesh=mesh)
 
 
 def parse_args():
