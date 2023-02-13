8ab02cbca5f20906c7361ab471a774e5a8bb7792
MURAKI Taichi <muraki.taichi@gmail.com>
2023-02-13 14:56:22 +0900

    f

--- src/modeling/model/modeling_fastmetro.py
+++ src/modeling/model/modeling_fastmetro.py
@@ -430,14 +430,18 @@ class MyModel(nn.Module):
         num_dec_layers = 3
         # configurations for the first transformer
         self.transformer_config_3 = {
-            "model_dim": 128,
+            "model_dim": 32,
             "dropout": args.transformer_dropout,
             "nhead": args.transformer_nhead,
-            "feedforward_dim": 512,
+            "feedforward_dim": 32 * 4,
             "num_enc_layers": num_enc_layers,
             "num_dec_layers": num_dec_layers,
             "pos_type": args.pos_type,
         }
+
+        # self.dim_reduce_enc_cam = nn.Linear(128, self.transformer_config_3["model_dim"])
+        self.dim_reduce_enc_img = nn.Linear(128, self.transformer_config_3["model_dim"])
+        self.dim_reduce_dec = nn.Linear(128, self.transformer_config_3["model_dim"])
 
         # build transformers
         self.transformer_3_decoder = build_transformer(self.transformer_config_3).decoder
@@ -481,6 +485,15 @@ class MyModel(nn.Module):
     def forward(self, cam_features_2, enc_img_features_2, jv_features_2):
         device = cam_features_2.device
         batch_size = cam_features_2.size(1)
+
+        # progressive dimensionality reduction
+        # reduced_cam_features_2 = self.dim_reduce_enc_cam(cam_features_2)  # 1 X batch_size X 32
+        reduced_enc_img_features_2 = self.dim_reduce_enc_img(
+            enc_img_features_2
+        )  # 49 X batch_size X 32
+        jv_features_2 = self.dim_reduce_dec(jv_features_2)
+        # (num_joints + num_vertices) X batch_size X 32
+
         # fastmetro:cam_features_3: torch.Size([1, 1, 128])
         # fastmetro:enc_img_features_3: torch.Size([49, 1, 128])
         # fastmetro:jv_features_3: torch.Size([216, 1, 128])
@@ -496,20 +509,20 @@ class MyModel(nn.Module):
         r_tokens_and_jv = torch.cat([r_tokens, jv_features_2], dim=0)
         # print(f"forward:r_tokens_and_jv: {r_tokens_and_jv.shape}")
         jv_features_final = self._do_decode(
-            h * w, batch_size, device, enc_img_features_2, r_tokens_and_jv, pos_enc_3
+            h * w, batch_size, device, reduced_enc_img_features_2, r_tokens_and_jv, pos_enc_3
         )
         # print(f"jv_features_final: {jv_features_final.shape}")
         # cam_features, enc_img_features, jv_features = self.transformer_3(
         #     enc_img_features_2, cam_features_2, jv_features_2, pos_enc_3
         # )
         # pred_cam = self.cam_predictor(cam_features_2).view(batch_size, 3)  # batch_size X 3
-        center_features = jv_features_final[0:3, :, :].transpose(0, 1)
-        center_features = center_features.contiguous().view(-1, 128 * 3)
+        center_features = jv_features_final[[0], :, :].transpose(0, 1)
+        # center_features = center_features.contiguous().view(-1, 128 * 3)
         center = self.ring_center_regressor(center_features)
-        normal_v_features = jv_features_final[3:6, :, :].transpose(0, 1)
-        normal_v_features = normal_v_features.contiguous().view(-1, 128 * 3)
+        normal_v_features = jv_features_final[[1], :, :].transpose(0, 1)
+        # normal_v_features = normal_v_features.contiguous().view(-1, 128 * 3)
         normal_v = self.ring_normal_regressor(normal_v_features)
-        radius_features = jv_features_final[[6], :, :].transpose(0, 1)
+        radius_features = jv_features_final[[2], :, :].transpose(0, 1)
         radius = self.radius_regressor(radius_features)
         return (
             center.squeeze(1),
