8f942751609edb46b3012d606f27cbe20decd9d8
MURAKI Taichi <muraki.taichi@gmail.com>
2023-02-14 18:00:31 +0900

    f

--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@ PYTHONPATH=. python scripts/custom/train.py \
     --fastmetro_resume_checkpoint "models/fastmetro_checkpoint/FastMETRO-L-H64_freihand_state_dict.bin" \
     --ring_info_pkl_rootdir "../ring_info/" \
     --lr 0.00018 \
+    --gamma 0.99 \
     --mymodel_resume_dir "models/checkpoint-15/" \
     --batch_size 32
 
