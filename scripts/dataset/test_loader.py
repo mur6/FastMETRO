17d8982fd89c11b2aada6ae12babbd6412b1d755
MURAKI Taichi <muraki.taichi@gmail.com>
2023-02-14 13:13:05 +0900

    f

--- scripts/dataset/test_loader.py
+++ scripts/dataset/test_loader.py
@@ -27,26 +27,9 @@ from src.modeling.model.transformer import build_transformer
 from src.handinfo.parser import train_parse_args
 
 # from src.handinfo.data import get_mano_faces
+from src.handinfo.mano import ManoWrapper
 from src.handinfo.data.tools import make_hand_data_loader
 from src.handinfo.utils import load_model_from_dir
-
-
-class ManoWrapper:
-    def __init__(self, *, mano_model):
-        self.mano_model = mano_model
-
-    def get_jv(self, *, pose, betas, adjust_func=None):
-        # pose = pose.unsqueeze(0)
-        # betas = betas.unsqueeze(0)
-        gt_vertices, gt_3d_joints = self.mano_model.layer(pose, betas)
-        if adjust_func is not None:
-            gt_vertices, gt_3d_joints = adjust_func(gt_vertices, gt_3d_joints)
-        return gt_vertices, gt_3d_joints
-
-    def get_trimesh_list(self, gt_vertices):
-        mano_faces = self.mano_model.layer.th_faces
-        # mesh objects can be created from existing faces and vertex data
-        return [trimesh.Trimesh(vertices=gt_vert, faces=mano_faces) for gt_vert in gt_vertices]
 
 
 def parse_args():
