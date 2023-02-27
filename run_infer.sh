51100bf09ce668b3322afb071c734bc7d6533bb7
MURAKI Taichi <muraki.taichi@gmail.com>
2023-02-27 12:01:02 +0900

    Fix run script

--- run_infer.sh
+++ run_infer.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
-PYTHONPATH=. python scripts/tests/test_load_mymodel_and_infer.py \
-    --train_yaml "../orig-MeshGraphormer/freihand/train.yaml" \
-    --val_yaml "../orig-MeshGraphormer/freihand/test.yaml" \
-    --ring_info_pkl_rootdir data/ring_info/ \
-    --fastmetro_resume_checkpoint "ckpt/FastMETRO-L-H64_freihand_state_dict.bin" \
-    --mymodel_resume_dir "/Users/taichi.muraki/out/2023/012/checkpoint-84" \
-    --device cpu
 
-#     --mymodel_resume_dir "/Users/taichi.muraki/out/2023/011/checkpoint-15/" \
+if [ "$1" = "abc" ]; then
+    echo "Equal 30"
+else
+    PYTHONPATH=. python scripts/tests/test_load_mymodel_and_infer.py \
+        --train_yaml "../orig-MeshGraphormer/freihand/train.yaml" \
+        --val_yaml "../orig-MeshGraphormer/freihand/test.yaml" \
+        --ring_info_pkl_rootdir data/ring_info/ \
+        --fastmetro_resume_checkpoint "ckpt/FastMETRO-L-H64_freihand_state_dict.bin" \
+        --mymodel_resume_dir "/Users/taichi.muraki/out/2023/012/checkpoint-84" \
+        --device cpu
+    #     --mymodel_resume_dir "/Users/taichi.muraki/out/2023/011/checkpoint-15/" \
+fi
 
 
 # PYTHONPATH=. python scripts/dataset/test_loader.py \
