bf90e1e6a7b191834c758c6c4fad9d691ee6f8a2
MURAKI Taichi <muraki.taichi@gmail.com>
2023-04-26 17:42:12 +0900

    Add run script.

---
+++ run.sh
@@ -0,0 +1,15 @@
+python -m torch.distributed.launch --nproc_per_node=1 \
+ src/tools/run_fastmetro_handmesh.py \
+ --train_yaml temp/freihand/train.yaml \
+ --val_yaml temp/freihand/test.yaml \
+--arch resnet50 \
+--model_name FastMETRO-L \
+--num_workers 4 \
+--per_gpu_train_batch_size 16 \
+--per_gpu_eval_batch_size 16 \
+--lr 1e-4 \
+--num_train_epochs 200 \
+--output_dir FastMETRO_outputs \
+--saving_epochs 1 \
+--resume_epoch 2 \
+--resume_checkpoint FastMETRO-small/checkpoint-2-16280/state_dict.bin
